8590a4e8d3c9bbf1edc1cd91842893f4beb89a08
aartiphapale93 <113004615+aartiphapale93@users.noreply.github.com>
2022-09-07 12:23:55 +0530

    Add files via upload

---
+++ Leapyear1.java
@@ -0,0 +1,22 @@
+package helloWorldproject;
+
+public class Leapyear1 {
+
+	public Leapyear1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		 int i=2022;
+		   if( i%4 ==0)
+		   {
+			   System.out.println("it is leap year");
+		   }
+		   else {
+			   System.out.println("it is not leap year");
+			   
+		   }
+		}
+
+	}
+
