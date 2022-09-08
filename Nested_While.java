585ba137c263435a1017d12212fbdcb553b06985
aartiphapale93 <113004615+aartiphapale93@users.noreply.github.com>
2022-09-08 12:49:01 +0530

    Add files via upload

---
+++ Nested_While.java
@@ -0,0 +1,23 @@
+
+public class Nested_While {
+
+	public static void main(String[]args){
+     int i=2;
+     while(i<=5)
+{
+int j=1;
+
+while(j<=10)
+{
+ System.out.print(""+(i*j));//2*1=2 j++;
+}
+System.out.println();
+
+i++;
+}
+}
+
+
+	}
+
+
